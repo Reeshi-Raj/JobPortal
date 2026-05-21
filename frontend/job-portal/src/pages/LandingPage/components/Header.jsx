880223044ba6a5cb8294c2880ec1f5d79a49e97a
Reeshi-Raj <reeshiraj707@gmail.com>
2026-05-21 21:09:26 +0530

    Added Hero Section

--- frontend/job-portal/src/pages/LandingPage/components/Header.jsx
+++ frontend/job-portal/src/pages/LandingPage/components/Header.jsx
@@ -9,7 +9,12 @@ const Header = () => {
     const navigate = useNavigate();
 
   return (
-    <header>
+    <motion.header
+        initial={{y: -20, opacity: 0}}
+        animate={{y: 0, opacity: 1}}
+        transition={{duration: 0.6}}
+        className="fixed top-0 left-0 right-0 z-50 bg-white/95 backdrop-blur-sm border-b border-gray-100 "
+    >
         <div className="container mx-auto px-4">
             <div className="flex items-center justify-between h-16">
                 {/*logo */}
@@ -63,7 +68,7 @@ const Header = () => {
                 </div>
             </div>
         </div>
-    </header>
+    </motion.header>
   )
 }
 
